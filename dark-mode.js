95d6bf4d36490b95a0664aac08c422f3560eba86
M Salma <principiorum@pm.me>
2023-01-08 20:06:09 +0700

    dark mode feature

---
+++ dark-mode.js
@@ -0,0 +1,39 @@
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  console.log(tab);
+  return tab;
+}
+
+function darkMode(params) {
+  const attr = document.documentElement.getAttribute("data-read-y-theme");
+  console.log(document.getElementById("dark-mode-button"));
+  if (attr === "dark") {
+    document.documentElement.setAttribute("data-read-y-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-read-y-theme", "dark");
+  }
+}
+
+let isDark = false;
+const darkmodebutton = document.getElementById("dark-mode-button");
+darkmodebutton.addEventListener("click", async function () {
+  const tb = await getCurrentTab();
+  const status = darkmodebutton.getElementsByTagName("span")[0];
+
+  if (isDark) {
+    status.innerText = "OFF";
+    status.style.color = "#262626";
+    isDark = false;
+  } else {
+    status.innerText = "ON";
+    status.style.color = "#4d7c0f";
+    isDark = true;
+  }
+
+  chrome.scripting.executeScript({
+    target: { tabId: tb.id },
+    func: darkMode,
+  });
+});
